3747981816c7adedf2b3b69deea7427dd3a5371c
Joris Bertier <joris.bertier@gmail.com>
2024-04-19 13:35:25 -0500

    exercice

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
 
 
-
-
-// let motUtilisateur = prompt("Entrez un mot correct pour scoré:" + listeMots[0])
-// if( motUtilisateur === listeMots[0]) {
-//     score++
-// }
-
-// motUtilisateur = prompt("Entrez un mot correct pour scoré:" + listeMots[1])
-// if( motUtilisateur === listeMots[1]) {
-//     score++
-// }
-// motUtilisateur = prompt("Entrez un mot correct pour scoré:" + listeMots[2])
-// if( motUtilisateur === listeMots[2]) {
-//     score++
-// }
-let listeMots = ["Cachalot", "Pétunia", "Serviette"]
-let score = 0
-
-for(i = 0; i < listeMots.length; i++) {
-    let motUtilisateur = prompt("Entrez un mot correct pour scoré:" + listeMots[i])
-    if( motUtilisateur === listeMots[i]) {
-            score++
-        }
+function afficherResultat(resultat, totalMots) {
+    let message = "Votre score est de "+ resultat +" sur " + totalMots
+    return message
 }
 
-console.log(score)
+function choisirPhrasesOuMots() {
+    let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+    while (choix !== "mots" && choix !== "phrases") {
+        choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+    }
+    return choix
+}
 
+function lancerBoucleDeJeu(listeProposition) {
+    let score = 0
+        for (let i = 0; i < listeProposition.length; i++) {
+            let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
+            if (motUtilisateur === listeProposition[i]) {
+                score++
+            }
+        }
+    return score
+} 
 
+function lancerJeu() {
+    let choix = choisirPhrasesOuMots()
+    let score = 0
+    let totalMots = 0
 
+    if(choix === "mots") {
+        lancerBoucleDeJeu(listeMots)
+        totalMots = listeMots.length
+    } else {
+        lancerBoucleDeJeu(listePhrases)
+        totalMots = listePhrases.length
+    }
+
+    console.log(afficherResultat(score, totalMots))
+    
+}
+
+lancerJeu()
 
 // switch(motUtilisateur) {
 //     case "Cachalot":
