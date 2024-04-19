599c5d29e75dd3615a89145d93f4d463b8c63c5d
Joris Bertier <joris.bertier@gmail.com>
2024-04-19 10:15:01 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let motUtilisateur = prompt("Entrez un mot :")
+
+let listeMots = ["Cachalot", "Pétunia"]
+switch (motUtilisateur) {
+    case "salut":
+        console.log('salut')
+        break
+    case "test":
+        console.log('test')
+        break
+}
