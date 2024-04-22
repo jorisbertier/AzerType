3a0c932cf6042d66a4d033127bf704db30c13d30
Joris Bertier <joris.bertier@gmail.com>
2024-04-22 10:00:29 -0500

    add style

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 lancerJeu()
 
-let inputEcriture = document.getElementById('inputEcriture')
+let inputEcriture = document.getElementById('inputEcriture');
+let btnValiderMot = document.getElementById('btnValiderMot');
+
+let zoneProposition = document.querySelector('.zoneProposition')
+let zoneScore = document.querySelector('.zoneScore span')
+
+let  = document.querySelectorAll('.optionSource input')
