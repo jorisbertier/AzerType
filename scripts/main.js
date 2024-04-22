b620f630a74d6602563cd9b8b1bf31664ac26c69
Joris Bertier <joris.bertier@gmail.com>
2024-04-22 10:34:04 -0500

    add

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,8 @@ let zoneProposition = document.querySelector('.zoneProposition')
 let zoneScore = document.querySelector('.zoneScore span')
 
 let  = document.querySelectorAll('.optionSource input')
+
+let myButton = document.getElementById("myButton");
+myButton.addEventListener("click", function() {
+    this.classList.toggle("active");
+});
